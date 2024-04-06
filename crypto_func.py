1e727cfcc77cf16044b5785c559ece1147c71001
longchau006 <104685813+longchau006@users.noreply.github.com>
2024-04-06 16:33:39 +0700

    Yêu cầu A (done)

--- crypto_func.py
+++ crypto_func.py
@@ -1,5 +1,4 @@
 # pip install pycryptodome
-
 import os
 import json
 from Cryptodome.Cipher import AES
@@ -10,64 +9,38 @@ def generateKeyAES(keySize):
     key = os.urandom(int(keySize) // 8)
     return key
 
+def encrypt_file_aes(inputFile, outputFile, key):
+  # đọc file nhị phân
+  plaintexts  = open(inputFile, "rb").read()
+  cipher = AES.new(key, AES.MODE_CTR) # dùng mode ctr: mode mạnh nhất
+  ctBytes = cipher.encrypt(plaintexts)
+  nonce = b64encode(cipher.nonce).decode('utf-8')
+  ciphertexts = b64encode(ctBytes).decode('utf-8')
+  result = json.dumps({'nonce':nonce, 'ciphertexts':ciphertexts})
+
+
+  with open(outputFile, "wb") as f:
+    f.write(result.encode())
+
+def decrypt_file_aes(inputFile, outputFile, key):
+  with open(inputFile, "rb") as f:
+    ctBytes = f.read()
+
+  try:
+    b64 = json.loads(ctBytes.decode())
+    nonce = b64decode(b64['nonce'])
+    ciphertexts = b64decode(b64['ciphertexts'])
+    cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+    print('type: ', type(cipher))
+    message = cipher.decrypt(ciphertexts)
+    print("The message was: ", message)
+  except (ValueError, KeyError):
+    print("Incorrect decryption")
+  with open(outputFile, "wb") as f:
+    f.write(message)
+
 keySize = input('Enter ASE key size (bit): ')
 key = generateKeyAES(keySize)
 print('key: ', key.hex())
-
-def encrypt_file_aes(input_file, output_file, key):
-  """
-  Mã hóa tập tin sử dụng thuật toán AES.
-
-  Tham số:
-    input_file (str): Đường dẫn đến tập tin đầu vào
-    output_file (str): Đường dẫn đến tập tin đầu ra
-    key (bytes): Khóa bí mật Ks
-
-  Trả về:
-    None
-  """
-  #cipher = AES.new(key, AES.MODE_)
-  #nonce = cipher.nonce
-  #ciphertext, tag = cipher.encrypt_and_digest(open(input_file, "rb").read())
-
-  plaintexts  = open(input_file, "rb").read()
-  cipher = AES.new(key, AES.MODE_CTR)
-  ct_bytes = cipher.encrypt(plaintexts)
-  nonce = b64encode(cipher.nonce).decode('utf-8')
-  ct = b64encode(ct_bytes).decode('utf-8')
-  result = json.dumps({'nonce':nonce, 'ciphertext':ct})
-
-
-  with open(output_file, "wb") as f:
-    f.write(result.encode())
-
-def decrypt_file_aes(input_file, output_file, key):
-  """
-  Giải mã tập tin sử dụng thuật toán AES.
-
-  Tham số:
-    input_file (str): Đường dẫn đến tập tin đầu vào
-    output_file (str): Đường dẫn đến tập tin đầu ra
-    key (bytes): Khóa bí mật Ks
-
-  Trả về:
-    None
-  """
-  with open(input_file, "rb") as f:
-    ct_bytes = f.read()
-
-  try:
-    b64 = json.loads(ct_bytes.decode())
-    nonce = b64decode(b64['nonce'])
-    ct = b64decode(b64['ciphertext'])
-    cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
-    print('type: ', type(cipher))
-    pt = cipher.decrypt(ct)
-    print("The message was: ", pt)
-  except (ValueError, KeyError):
-    print("Incorrect decryption")
-  with open(output_file, "wb") as f:
-    f.write(pt)
-
-encrypt_file_aes('D:\\Network Security\\plaintext.txt', 'D:\\Network Security\\ciphertext.txt', key)
-decrypt_file_aes('D:\\Network Security\\ciphertext.txt', 'D:\\Network Security\\message.txt', key)
+encrypt_file_aes('plaintext.txt', 'ciphertext.txt', key)
+decrypt_file_aes('ciphertext.txt', 'message.txt', key)
