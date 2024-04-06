14a51da3894470b897355f9a69b8d77ae828962a
HoangTu47 <haquocbao607@gmail.com>
2024-04-06 10:25:30 +0700

    almost finish decrypt.py.

---
+++ decrypt.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+# module for file manipulation
+import os
+
+# module for TextIO handle
+import sys
+
+# user-defined cryptophic tool module
+import crypto_func
+
+# module to handle command-line argument
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Decrypt an encrypted file.',
+    epilog='Made with love by Quoc Bao & Chau Long!'
+)
+
+parser.add_argument('infile',
+    type=argparse.FileType('r'),
+    help='File path of encrypted file.'
+)
+
+parser.add_argument( "-k", "--kprivatefile",
+    nargs='?',
+    type=argparse.FileType('r'),
+    default=sys.stdin,
+    help='File path of file storing the Kprivate key.'
+)
+
+parser.add_argument( "-o", "--outfile",
+    nargs='?',
+    type=argparse.FileType('w'),
+    default=sys.stdout,
+    help='Desired file path for the decrypted file.'
+)
+
+args = parser.parse_args()
+
+# --- subsection a. ---
+
+with args.infile as file:
+    # read file content
+    encrpyted_data = file.read()
+
+# --- subsection b. ---
+
+# extract Kprivate key
+# either from file or user input
+with args.kprivatefile as file:
+    KPrivate = file.readline().strip()
+    print(KPrivate)
+
+# --- subsection c. ---
+
+# Key processing
+# TODO
+
+# --- subsection d. ---
+
+# Key processing
+# TODO
+
+# --- subsection e. ---
+with args.outfile as file:
+    file.write("hello world!\n")
